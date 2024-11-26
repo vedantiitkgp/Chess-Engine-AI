1a1995d9ba63c9efaba1b6b5a07e6736bfb888e2
vedantiitkgp <vedantntpc@gmail.com>
2024-11-26 00:20:45 -0800

    Added apply move function

--- chess_agent.cpp
+++ chess_agent.cpp
@@ -26,7 +26,7 @@ enum Peice {
 typedef struct {
     int squares[64]; // Array representing value of squares
     int to_move; // 1 fo white and -1 for black
-    int castling_rights; // Bitmask for castling rights
+    int castling_rights; // Bitmask for Castling rights [0, 1 bit for white, 2, 3 bit for black]
     int en_passant; // En passant target square (0-63) or -1
     int halfmove_clock;
     int fullmove_number;
@@ -207,13 +207,16 @@ int generate_moves(Board *board, Move *moves){
 
                         // Castling(Simplified doesnt check that if squares under attack)
                         if((side == 1 && peice == W_KING) && (side == -1 && peice == B_KING)){
-                            if(board->castling_rights && ((side == 1 && start == 4) || (side == -1 && start == 60))){
-                                // Kingside Castling
+                            // Kingside Castling
+                            if (board->castling_rights & (side == 1 ? 0x1 : 0x4)) {
                                 if((board->squares[start + 1] == EMPTY) && (board->squares[start + 2] == EMPTY) && (board->squares[start+3] == ((side == 1) ? W_ROOK : B_ROOK))){
                                     moves[num_moves] = (Move){start, start + 2, peice, EMPTY, EMPTY, 1, 0, board->en_passant, board->castling_rights, board->halfmove_clock };
                                     num_moves++;
                                 }
-                                // Queenside Castling
+                            }
+
+                            // Queenside Castling
+                            if(board->castling_rights & (side == 1 ? 0x2 : 0x8)){
                                 if((board->squares[start - 1] == EMPTY) && (board->squares[start - 2] == EMPTY) && (board->squares[start - 3] == EMPTY) && (board->squares[start-4] == ((side == 1) ? W_ROOK : B_ROOK))){
                                     moves[num_moves] = (Move){start, start - 2, peice, EMPTY, EMPTY, 1, 0, board->en_passant, board->castling_rights, board->halfmove_clock };
                                     num_moves++;
@@ -228,7 +231,80 @@ int generate_moves(Board *board, Move *moves){
     return num_moves;
 }
 
+void apply_move(Board *board, Move *move){
+    //Save Previous State
+    move->prev_en_passant = board->en_passant;
+    move->prev_castling_rights = board->castling_rights;
+    move->prev_halfmove_clock = board->halfmove_clock;
 
+    board->en_passant = -1;
+
+    //Move the Peice
+    board->squares[move->to] = move->peice;
+    board->squares[move->from] = EMPTY;
+
+    //Handle Captures
+    if(move->captures != EMPTY){
+        board->halfmove_clock = 0;
+    }
+
+    //Handle double pawn move (set en passant target)
+    if(((move->peice == W_PAWN) || (move->peice == B_PAWN)) && abs(move->from - move->to) == 16 ){
+        board->en_passant = (move->from + move-> to)/2;
+    }
+
+    //Handling en_passant pawn capture
+    if(move->is_en_passant){
+        int captured_pawn_pos = move_to + (board->to_move) ? 8 : - 8;
+        board->squares[captured_pawn_pos] = EMPTY;
+    }
+
+    //Handling Pawn promotions
+    if(move->promotion != EMPTY){
+        board->squares[move->to] = move->promotion;
+    }
+
+    // Handle Castling_rights
+    if (move->piece == W_KING) {
+        board->castling_rights &= ~0x3; // Clear white castling rights
+    } else if (move->piece == B_KING) {
+        board->castling_rights &= ~0xC; // Clear black castling rights
+    } else if (move->piece == W_ROOK) {
+        if (move->from == 7) board->castling_rights &= ~0x1; // H1 rook moved
+        else if (move->from == 0) board->castling_rights &= ~0x2; // A1 rook moved
+    } else if (move->piece == B_ROOK) {
+        if (move->from == 63) board->castling_rights &= ~0x4; // H8 rook moved
+        else if (move->from == 56) board->castling_rights &= ~0x8; // A8 rook moved
+    }
+
+    //Handle Castling Move
+    if(move->is_castling){
+        if(move->to == move->from + 2){
+            //Kingside castling
+            board->squares[move->from + 1] = board->squares[move->from + 3];
+            board->squares[move->from + 3] = EMPTY;
+        }
+        else if(move->to == move->from - 2){
+            board->squares[move->from -1] = board->squares[move->from-4];
+            board->squares[move->from-4] = EMPTY;
+        }
+    }
+
+    //Update Turn
+    board->to_move = - board->to_move;
+
+    //Update Halfmove Clock
+    if(move->piece == W_PAWN || move->piece == B_PAWN || move->captured != EMPTY){
+        board->halfmove_clock = 0;
+    }
+    else{
+        board->halfmove_clock++;
+    }
+
+    if(board->to_move == 1){
+        board->fullmove_number++;
+    }
+}
 
 
 
